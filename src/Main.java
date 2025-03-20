b5706b995ed6026198f075d9cb4e4d9e03a9325c
causicb <benjo1306@gmail.com>
2025-03-20 19:17:39 +0100

    More clear solution for " " input between reversed words

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+
         Scanner input = new Scanner(System.in);
         String inputString = input.nextLine();
 
@@ -12,9 +13,12 @@ public class Main {
 
         StringBuilder reverseText = new StringBuilder();
         for (int i = words.length - 1; i >= 0; i--) {
-            reverseText.append(words[i]).append(" ");
+            reverseText.append(words[i]);
+            reverseText.append(" ");
         }
+
         System.out.println(reverseText.toString().trim());
+
     }
 
 }
