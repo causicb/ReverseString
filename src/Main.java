7734c5306a912208687448d1caa8505b98d5fc88
causicb <benjo1306@gmail.com>
2025-03-20 19:05:45 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String inputString = input.nextLine();
+
+        String[] words = inputString.split(" ");
+
+        StringBuilder reverseText = new StringBuilder();
+        for (int i = words.length - 1; i >= 0; i--) {
+            reverseText.append(words[i]).append(" ");
+        }
+        System.out.println(reverseText.toString().trim());
+    }
+
+}
